a29d26b1e9ae32e4c2bbe651edf23487fac170d6
omaoann <omaokerubo21@gmail.com>
2018-12-05 16:38:19 +0300

    [ft #162445485] add landing page to default route

--- run.py
+++ run.py
@@ -3,5 +3,12 @@ from app import create_app
 
 app = create_app(config_name="development")
 
+@app.route('/')
+def landing_page():
+    
+    return "Welcome to iReporter.\n\
+      A place to report all corruption cases\n\
+      and incidents that require interventions"
+
 if __name__ =='__main__':
     app.run(debug=True)
