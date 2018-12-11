2db7811e981a22f06387edabb29c66b4d7469f00
omaoann <omaokerubo21@gmail.com>
2018-12-11 11:49:08 +0300

    [feature #162409846] add resource url for creating a new incident

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -2,6 +2,7 @@
 from flask_restful import Api, Resource
 from flask import Blueprint
 from .viewsV2.user_views import Register, Login
+from .viewsV2.incident_view import AddRecord
 
 
 version_two = Blueprint('api_v2',__name__,url_prefix='/api/v2/')
@@ -10,3 +11,4 @@ api = Api(version_two)
 
 api.add_resource(Register, 'signup')
 api.add_resource(Login, 'login')
+api.add_resource(AddRecord, 'interventions')
