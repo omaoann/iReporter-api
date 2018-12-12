a58fe7aae68f15ee80eaf28f4cee08cd944073b4
omaoann <omaokerubo21@gmail.com>
2018-12-12 18:45:47 +0300

    [chore #162409335]add tests for the get endpoint

--- app/tests/v2/test_records.py
+++ app/tests/v2/test_records.py
@@ -12,22 +12,19 @@ class Records(unittest.TestCase):
         create_tables()
         self.app = create_app(config_name='testing')
         self.client = self.app.test_client()
+        self.signup = self.client.post("api/v2/signup", 
+                data=json.dumps(registration_data),
+                content_type="application/json") 
+        self.login = self.client.post("api/v2/login",
+                data=json.dumps(data_login), 
+                content_type="application/json")
+        self.token = json.loads(self.login.data)["token"]
 
     def test_create_incidents(self):
         """This method tests for creation of a new incident"""
-        response = self.client.post("api/v2/signup", 
-                data=json.dumps(registration_data),
-                content_type="application/json")           
-        self.assertEqual(response.status_code,201)
-
-        response1 = self.client.post("api/v2/login",
-                data=json.dumps(data_login), 
-                content_type="application/json")
-        token = json.loads(response1.data)["token"]
-
         response2 = self.client.post("api/v2/interventions",
              data=json.dumps(record_data), 
-             headers=dict(Authorization='Bearer '+token),
+             headers=dict(Authorization='Bearer '+ self.token),
              content_type="application/json")
         result = json.loads(response2.data)
         self.assertEqual(result["Data"][0]["message"],"Created record successfully")
@@ -35,19 +32,10 @@ class Records(unittest.TestCase):
 
     def test_empty_field(self):
         """This method tests if a user has left a field empty"""
-        response = self.client.post("api/v2/signup", 
-                data=json.dumps(registration_data),
-                content_type="application/json")           
-        self.assertEqual(response.status_code,201)
-
-        response1 = self.client.post("api/v2/login",
-                data=json.dumps(data_login), 
-                content_type="application/json")
-        token = json.loads(response1.data)["token"]
 
         response2 = self.client.post("api/v2/interventions",
              data=json.dumps(reccord_empty_field), 
-             headers=dict(Authorization='Bearer '+token),
+             headers=dict(Authorization='Bearer '+ self.token),
              content_type="application/json")
         result = json.loads(response2.data)
         self.assertEqual(result["message"],"Please fill all fields")
@@ -57,22 +45,63 @@ class Records(unittest.TestCase):
         """This method tests if location coordinates are within
                  specified points"""
 
-        response = self.client.post("api/v2/signup", 
-                data=json.dumps(registration_data),
-                content_type="application/json")           
-        self.assertEqual(response.status_code,201)
-
-        response1 = self.client.post("api/v2/login",
-                data=json.dumps(data_login), 
-                content_type="application/json")
-        token = json.loads(response1.data)["token"]
-
         response2 = self.client.post("api/v2/interventions",
              data=json.dumps(reccord_wrong_location), 
-             headers=dict(Authorization='Bearer '+token),
+             headers=dict(Authorization='Bearer '+ self.token),
              content_type="application/json")
         result = json.loads(response2.data)
-        self.assertEqual(result["message"],"Location does not exist")              
+        self.assertEqual(result["message"],"Location does not exist")  
+
+###test get methods
+    def test_get_all_data(self):
+        """Test it gets all data that has been posted"""
+
+        self.client.post("api/v2/interventions",
+             data=json.dumps(record_data), 
+             headers=dict(Authorization='Bearer '+ self.token),
+             content_type="application/json")
+        self.client.post("api/v2/interventions",
+             data=json.dumps(record_data), 
+             headers=dict(Authorization='Bearer '+ self.token),
+             content_type="application/json")
+        response = self.client.get("api/v2/interventions",
+             headers=dict(Authorization='Bearer '+ self.token),
+             content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_single_record(self):
+        """Test it gets a single record based on id"""
+        self.client.post("api/v2/interventions",
+             data=json.dumps(record_data), 
+             headers=dict(Authorization='Bearer '+ self.token),
+             content_type="application/json")
+        response = self.client.get("api/v2/interventions/1",
+             headers=dict(Authorization='Bearer '+ self.token),
+             content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+            
+    def test_get_empty_list(self):
+        """Test it returns error message if no records exist"""  
+        response = self.client.get("api/v2/interventions",
+             headers=dict(Authorization='Bearer '+ self.token),
+             content_type="application/json")
+        self.assertEqual(response.status_code, 404)
+        result = json.loads(response.data)
+        self.assertEqual(result["Message"],"There are no records")
+
+    def test_non_existing_id(self):
+        """Test it returns message if id does not exist"""
+        self.client.post("api/v2/interventions",
+             data=json.dumps(record_data), 
+             headers=dict(Authorization='Bearer '+ self.token),
+             content_type="application/json")
+        response = self.client.get("api/v2/interventions/16781",
+             headers=dict(Authorization='Bearer '+ self.token),
+             content_type="application/json")
+        self.assertEqual(response.status_code, 404)
+        result = json.loads(response.data)
+        self.assertEqual(result["Message"],"Record does not exist")
+                        
 
     def tearDown(self):
         drop_tables()
