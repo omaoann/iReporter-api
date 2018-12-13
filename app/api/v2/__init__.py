c93c7e13bc0ac7467fc7319ec1617772d3087216
omaoann <omaokerubo21@gmail.com>
2018-12-13 10:43:40 +0300

    [feature #162409915]add endpoint urls

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -2,7 +2,9 @@
 from flask_restful import Api, Resource
 from flask import Blueprint
 from .viewsV2.user_views import Register, Login
-from .viewsV2.incident_view import AddRecord
+from .viewsV2.incident_view import (AddRecord,
+                        GetRecord,EditComment,EditLocation)
+from .viewsV2.admin_views import StatusV
 
 
 version_two = Blueprint('api_v2',__name__,url_prefix='/api/v2/')
@@ -12,3 +14,7 @@ api = Api(version_two)
 api.add_resource(Register, 'signup')
 api.add_resource(Login, 'login')
 api.add_resource(AddRecord, 'interventions')
+api.add_resource(GetRecord, 'interventions/<int:id>')
+api.add_resource(EditComment, 'interventions/<int:id>/comment')
+api.add_resource(EditLocation, 'interventions/<int:id>/location')
+api.add_resource(StatusV, '<type>/<int:id>/status')
