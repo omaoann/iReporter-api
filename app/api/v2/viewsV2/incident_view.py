9e0c8c2833d53c1f5b3de7e7bd1f022576c94ef4
omaoann <omaokerubo21@gmail.com>
2018-12-13 10:42:34 +0300

    [feature #162409915]create views for the get records endpoint

--- app/api/v2/viewsV2/incident_view.py
+++ app/api/v2/viewsV2/incident_view.py
@@ -2,8 +2,8 @@ from flask import jsonify, request
 from flask_restful import Resource
 from ..modelsV2.incidents import Record
 
-class AddRecord(Resource):
-    """This class registers a new user"""
+class AddRecord(Resource,Record):
+    """This class registers a new user and gets all records"""
     
     def __init__(self):
         self.data = Record()
@@ -21,3 +21,18 @@ class AddRecord(Resource):
             },400
 
         return self.data.add_record(flag_type,location,comment)
+
+    def get(self):
+        """Get all records"""
+        return self.data.get_record()
+
+class GetRecord(Resource,Record):
+    """This class gets a single record and deletes a record"""
+    
+    def __init__(self):
+        self.data = Record()
+
+    def get(self,id):
+        """Get single record by id"""
+        return self.data.get_single_record(id)
+
