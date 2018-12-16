64ed637508b67a4eb15a0c891b0539add81335c7
omaoann <omaokerubo21@gmail.com>
2018-12-17 00:06:59 +0300

    [feature #162689060] add code to handle sending of emails to user after status update

--- app/api/v2/modelsV2/admin_models.py
+++ app/api/v2/modelsV2/admin_models.py
@@ -2,10 +2,10 @@ import psycopg2
 import os
 from db.db_config import init_db,close_connection
 from flask import jsonify, request
-from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity
-)
+from flask_jwt_extended import (jwt_required, 
+             create_access_token,get_jwt_identity
+              )
+import smtplib
 
 class Status():
     """This class is for editing status of a record"""
@@ -27,14 +27,21 @@ class Status():
                     "Status": 403,
                     "Message":"Unauthorized user"            
                       },403 
-            cur.execute("SELECT * FROM incidents WHERE \
+            cur.execute("SELECT user_id FROM incidents WHERE \
             incident_id = %s AND type = %s",(id,type))
             record = cur.fetchall()
             if not record:
                 return{
                     "Status": 404,
                     "Message": "Record does not exist"
-                },404  
+                },404 
+
+            user =record[0][0] 
+            cur.execute("SELECT email FROM users WHERE user_id = %s",(user,))
+            user_email = cur.fetchall()
+            email = user_email[0][0]
+            print(email)
+
             cur.execute("UPDATE incidents SET status = %s WHERE \
             incident_id = %s and type = %s \
             RETURNING incident_id,type,location,status,comment,user_id",
@@ -49,6 +56,16 @@ class Status():
                   "Status":updated_record[3],
                   "Comment":updated_record[4]
             }
+            #send mail
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(
+                    "projectemail2303@gmail.com", "Projectemail_2303")
+            msg = "Your {} record is now {}".format(type, status)
+            server.sendmail(
+                    "projectemail2303@gmail.com", email, msg)
+            server.quit()  
+            #update admin after status change          
             return{
                 "Status": 200,
                 "Message":"Updated " + type + " record status",
