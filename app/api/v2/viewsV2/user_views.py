dc1cd1e140f986e3acb5c0af6ce5c66ca951011e
omaoann <omaokerubo21@gmail.com>
2018-12-16 13:58:48 +0300

    [chore #162684325] add validations for user inputs

--- app/api/v2/viewsV2/user_views.py
+++ app/api/v2/viewsV2/user_views.py
@@ -1,6 +1,43 @@
 from flask import jsonify, request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from ..modelsV2.users import Users
+
+parser=reqparse.RequestParser(bundle_errors=True)
+parser.add_argument(
+    'firstname',type=str,
+       required=True,
+       help="Firstname field can not be left blank"
+       )
+parser.add_argument(
+    'lastname',type=str,
+       required=True,
+       help="Last name field can not be left blank"
+       )
+parser.add_argument(
+    'othername',type=str,
+       required=True,
+       help="Other name field can not be left blank"
+       )       
+parser.add_argument(
+    'username',type=str,
+       required=True,
+       help="Username field can not be left blank"
+       )
+parser.add_argument(
+    'email',type=str,
+       required=True,
+       help="Username field can not be left blank"
+       )
+parser.add_argument(
+    'phonenumber',type=int,
+       required=True,
+       help="Phone number field can not be left blank"
+       )   
+parser.add_argument(
+    'password',type=str,
+       required=True,
+       help="Password field can not be left blank"
+       )    
 
 class Register(Resource):
     """This class registers a new user"""
@@ -9,7 +46,7 @@ class Register(Resource):
         self.data = Users()
 
     def post(self):
-        details = request.get_json()
+        details = parser.parse_args()
         f_name = details['firstname']
         l_name = details['lastname']
         o_name = details['othername']
@@ -18,11 +55,19 @@ class Register(Resource):
         phone_no = details['phonenumber']
         password = details['password']
 
-        if not email or not password or not f_name or not phone_no:
+        if not email or not password or not f_name\
+         or not phone_no or not f_name or not l_name\
+         or not o_name:
             return {
-                "message": "Please fill all fields",
+                "message": "Please fill all required fields",
                 "status": 400
             },400
+        if f_name.isalpha() is False:
+            return {'message': 'Please enter a valid First name'}, 400
+        if l_name.isalpha() is False:
+            return {'message': 'Please enter a valid Last name'}, 400
+        if o_name.isalpha() is False:
+            return {'message': 'Please enter a valid other name'}, 400
 
         return self.data.save_user(f_name,o_name,l_name,username,\
                      email,phone_no,password)
